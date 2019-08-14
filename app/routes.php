253558171d26d3754e6f9bf81c85bdd16cc547a7
Johannes Pichler <johannes.pichler@karriere.at>
2019-08-14 06:32:31 +0200

    Add setup message

--- app/routes.php
+++ app/routes.php
@@ -5,7 +5,7 @@ use Psr\Http\Message\ResponseInterface;
 
 return function (App $app) {
     $app->get('/products', function (ServerRequestInterface $request, ResponseInterface $response) {
-        $response->getBody()->write('Hello World!');
-        return $response->withStatus(200);
+        $response->getBody()->write(json_encode(['setup' => 'success']));
+        return $response->withHeader('Content-Type', 'application/json');
     });
 };
