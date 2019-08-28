f6c1b398b50b2feae10a1226dcedf0703815765c
Johannes Pichler <johannes.pichler@karriere.at>
2019-08-28 14:19:17 +0200

    Fix middleware

--- README.md
+++ README.md
@@ -13,3 +13,8 @@ composer start
 ```
 
 To make sure everything is working out correctly, you need to access the following url: `http://localhost:8080/products`.
+
+## Resources
+
+* [Slim Framework](https://www.slimframework.com/docs/v4/)
+* [Laravel Eloquent](https://laravel.com/docs/5.8/eloquent)
--- src/Middlewares/MakeJsonApiRequest.php
+++ src/Middlewares/MakeJsonApiRequest.php
@@ -4,13 +4,14 @@ namespace App\Middlewares;
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 use WoohooLabs\Yin\JsonApi\Exception\DefaultExceptionFactory;
 use WoohooLabs\Yin\JsonApi\Request\JsonApiRequest;
 
-class MakeJsonApiRequest
+class MakeJsonApiRequest implements MiddlewareInterface
 {
-    public function __invoke(Request $request, RequestHandler $handler): Response
+    public function process(Request $request, RequestHandler $handler): Response
     {
         $exceptionFactory = new DefaultExceptionFactory;
         $jsonApiRequest = new JsonApiRequest($request, $exceptionFactory);
